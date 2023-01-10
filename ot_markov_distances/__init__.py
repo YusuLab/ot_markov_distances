97a0c9dc05832e7abffda6b24214eefb2efed0ab
Tristan BRUGÈRE <67050900+nephanth@users.noreply.github.com>
2023-01-10 12:47:41 +0100

    added sinkhorn_internal function

--- ot_markov_distances/__init__.py
+++ ot_markov_distances/__init__.py
@@ -1,2 +1,8 @@
 from .sinkhorn import sinkhorn
 from .wl import wl_k, markov_measure
+
+__all__: list[str] = [
+    "sinkhorn",
+    "wl_k", 
+    "markov_measure"
+]
--- ot_markov_distances/sinkhorn.py
+++ ot_markov_distances/sinkhorn.py
@@ -9,6 +9,56 @@ using the method described in :cite:`feydyInterpolatingOptimalTransport2019`
 import torch
 from torch import Tensor
 
+
+def sinkhorn_internal(a: Tensor, b: Tensor, C: Tensor, 
+                      epsilon: float, k: int=100):
+    """Same as sinkhorn, but returns f, g and P instead of the result
+    Beware, this method does not have the shortcut differentiation
+    
+    (It can still be differentiated by autograd though)
+
+    Args:
+        a: (*batch, n) vector of the first distribution
+        b: (*batch, m) vector of the second distribtion
+        C: (*batch, n, m) cost matrix
+        epsilon: regularisation term for sinkhorn
+        k: number of sinkhorn iterations (default 100)
+
+    Returns:
+        f: (*batch, n)
+        g: (*batch, m)
+        log_P: (*batch, n, m)
+    """
+    *batch, n = a.shape
+    *batch_, m = b.shape
+    *batch__, n_, m_ = C.shape
+    batch = torch.broadcast_shapes(batch, batch_, batch__)
+    assert n == n_
+    assert m == m_
+    log_a = a.log()[..., :, None]
+    log_b = b.log()[..., None, :]
+    mC_eps = - C / epsilon
+
+    f_eps = torch.randn((*batch, n, 1))#f over epsilon + log_a 
+    #batch, n
+    g_eps = torch.randn((*batch, 1, m))#g over epsilon + log_b
+    #batch, m
+    for _ in range(k):
+        f_eps =  - torch.logsumexp(mC_eps + g_eps + log_b, dim=-1, keepdim=True)
+        g_eps =  - torch.logsumexp(mC_eps + f_eps + log_a, dim=-2, keepdim=True)
+    log_P = mC_eps + f_eps + g_eps + log_a + log_b
+    # Note: we dont actually need save_for_backwards here
+    # we could say ctx.f_eps = f_eps etc.
+    # save_for_backwards does sanity checks 
+    # such as checking that the saved variables do not get changed inplace
+    # (if you have output them, which is not the case)
+    # see https://discuss.pytorch.org/t/how-to-save-a-list-of-integers-for-backward-when-using-cpp-custom-layer/25483/5
+    f = epsilon * f_eps.squeeze(-1)
+    g = epsilon * g_eps.squeeze(-2)
+    return f, g, log_P
+    #print(res, (f.squeeze(-1) * a).sum(-1) + (g.squeeze(-2) * b).sum(-1))
+
+
 class Sinkhorn(torch.autograd.Function):
     """Computes Sinkhorn divergence"""
     @staticmethod
@@ -30,43 +80,20 @@ class Sinkhorn(torch.autograd.Function):
 
         Returns:
             divergence: (*batch) $divergence[*i] = OT^\epsilon(a[*i], b[*i], C[*i])$
-
-            (f_eps, g_eps, log_P): only if return_solutions is True. 
-            f_eps is f/epsilon, g_eps is g/epsilon where f and g are the dual solutions. 
-            log_P is log(P) where P is the optimal transport
         """
 
-        *batch, n = a.shape
-        *batch_, m = b.shape
-        *batch__, n_, m_ = C.shape
-        batch = torch.broadcast_shapes(batch, batch_, batch__)
-        assert n == n_
-        assert m == m_
         with torch.no_grad():
-            log_a = a.log()[..., :, None]
-            log_b = b.log()[..., None, :]
-            mC_eps = - C / epsilon
-
-            f_eps = torch.randn((*batch, n, 1))#f over epsilon + log_a 
-            #batch, n
-            g_eps = torch.randn((*batch, 1, m))#g over epsilon + log_b
-            #batch, m
-            for _ in range(k):
-                f_eps =  - torch.logsumexp(mC_eps + g_eps + log_b, dim=-1, keepdim=True)
-                g_eps =  - torch.logsumexp(mC_eps + f_eps + log_a, dim=-2, keepdim=True)
-            log_P = mC_eps + f_eps + g_eps + log_a + log_b
-            # Note: we dont actually need save_for_backwards here
-            # we could say ctx.f_eps = f_eps etc.
-            # save_for_backwards does sanity checks 
-            # such as checking that the saved variables do not get changed inplace
-            # (if you have output them, which is not the case)
-            # see https://discuss.pytorch.org/t/how-to-save-a-list-of-integers-for-backward-when-using-cpp-custom-layer/25483/5
-            f = epsilon * f_eps.squeeze(-1)
-            g = epsilon * g_eps.squeeze(-2)
-            #print(res, (f.squeeze(-1) * a).sum(-1) + (g.squeeze(-2) * b).sum(-1))
+            f, g, log_P = sinkhorn_internal(a, b, C, epsilon, k)
             res = (f * a).sum(-1) + (g * b).sum(-1)
-            ctx.save_for_backward(f, g, log_P)
-            ctx.epsilon = epsilon
+
+        # Note: we dont actually need save_for_backwards here
+        # we could say ctx.f_eps = f_eps etc.
+        # save_for_backwards does sanity checks 
+        # such as checking that the saved variables do not get changed inplace
+        # (if you have output them, which is not the case)
+        # see https://discuss.pytorch.org/t/how-to-save-a-list-of-integers-for-backward-when-using-cpp-custom-layer/25483/5
+        ctx.save_for_backward(f, g, log_P)
+        ctx.epsilon = epsilon
 
         return res
     
@@ -88,7 +115,7 @@ class Sinkhorn(torch.autograd.Function):
         # g_eps (*batch, 1, m)
         # log_P (*batch, n, m)
         f, g, log_P = ctx.saved_tensors
-        epsilon = ctx.epsilon
+        # epsilon = ctx.epsilon
 
         d_a = f * grad_output[..., None]
         d_a = d_a - d_a.mean(-1, keepdim=True) # so that the gradient
