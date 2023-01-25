beed8345b3112793bf7e2d19f92a537ae0ac1225
Tristan BRUGÈRE <67050900+nephanth@users.noreply.github.com>
2023-01-25 10:27:25 -0800

    repr for some classes

--- experiments/utils/modules.py
+++ experiments/utils/modules.py
@@ -2,26 +2,36 @@ import torch
 from torch import nn, Tensor
 from torch.nn import functional as F
 
+from ml_lib.misc import auto_repr
+
+@auto_repr("size")
 class ParametricMarkovMatrix(nn.Module):
 
+    size:int
     m: nn.Parameter
     heat: float = 1.
 
     def __init__(self, size:int, *, heat:float = 1.):
         super().__init__()
+        self.size = size
         self.m = nn.Parameter(torch.randn(size, size))
         self.heat = heat
 
     def forward(self) -> Tensor:
         return F.softmax(self.m / self.heat, dim=-1)
 
+@auto_repr("size1", "size2")
 class ParametricMatching(nn.Module):
-
+    
+    size1: int
+    size2: int
     m: nn.Parameter
     heat: float = 1.
 
     def __init__(self, size1:int, size2: int, *, heat:float = 1.):
         super().__init__()
+        self.size1 = size1
+        self.size2 = size2
         self.m = nn.Parameter(torch.randn(size1, size2))
         self.heat = heat
 
