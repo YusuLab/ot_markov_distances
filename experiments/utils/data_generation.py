62242463f48553b8d7953bb80dfcf9fde23a2016
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2023-04-11 17:53:29 -0700

    er noise will work with directed grahps

--- experiments/utils/data_generation.py
+++ experiments/utils/data_generation.py
@@ -73,7 +73,7 @@ def add_er_noise(G, p):
     """
     G = nx.convert_node_labels_to_integers(G)
     G_matrix = nx.to_numpy_array(G)
-    noise = nx.to_numpy_array(nx.erdos_renyi_graph(len(G), p))
+    noise = nx.to_numpy_array(nx.erdos_renyi_graph(len(G), p, directed=G.is_directed()))
     new_G_matrix = np.logical_xor(G_matrix, noise)
     new_G = nx.create_empty_copy(G)
     edges = nx.from_numpy_array(new_G_matrix, create_using=type(G)).edges()
